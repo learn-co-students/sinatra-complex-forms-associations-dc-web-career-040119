542ffa6948612bf4ff470ab2a0268d35dfc066ea
github username <github email address>
2019-04-24 15:42:10 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,50 @@
 class PetsController < ApplicationController
 
+  set :views, 'app/views/pets'
+
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :index
   end
 
-  get '/pets/new' do 
-    erb :'/pets/new'
+  get '/pets/new' do
+    @owners = Owner.all
+    erb :new
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(name: params[:pet_name])
+    # binding.pry
+    if params[:owner_name].empty?
+      @owner = Owner.find(params[:owner_id])
+    else
+      @owner = Owner.create(name: params[:owner_name])
+    end
+    @owner.pets << @pet
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    erb :'/pets/show'
+    erb :show
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :edit
+  end
 
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    if params[:owner][:name].empty?
+      @owner = Owner.find(params[:owner][:id])
+    else
+      @owner = Owner.create(name: params[:owner][name])
+    end
+    @pet.update(name: params[:pet_name],
+                owner: @owner)
+    @pet.owner.update(name: params[:owner_name]) unless params[:owner_name].empty?
     redirect to "pets/#{@pet.id}"
   end
 end
